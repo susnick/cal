c2af9f53018e7d4529b928232ec58418692dec43
Scott Usnick <susnick@appliedtechres.com>
2019-08-22 15:45:10 -0400

    add gitignore and update script to download build tools of encironment does not have any

--- build.cmd
+++ build.cmd
@@ -1,16 +1,28 @@
 @echo off
-WHERE gcc>nul 2>nul
-IF %ERRORLEVEL% NEQ 0 goto runcl
 
-gcc cal.c -ocal
+REM :runcl
+REM WHERE cl>nul 2>nul
+REM IF %ERRORLEVEL% NEQ 0 goto rungcc
+REM cl cal.c
+REM rm cal.obj
+REM goto finish
+
+:rungcc
+::WHERE gcc>nul 2>nul
+::IF %ERRORLEVEL% NEQ 0 goto downloadtools
+%cd%\gcc cal.c -ocal
 goto finish
 
+:downloadtools
+echo You do not have any of the required build tools installed.
+echo Downloading build tools now.
+::powershell -Command "Invoke-WebRequest https://bitbucket.org/susnick/cal/downloads/mingw64.zip -OutFile mingw64.zip"
+bitsadmin /transfer downloadjob /dynamic /priority foreground https://bitbucket.org/susnick/cal/downloads/mingw64.zip %cd%\mingw64.zip
 
-
-:runcl
-cl cal.c
-rm cal.obj
-
+powershell Expand-Archive mingw64.zip
+rm mingw64.zip
+set PATH=%cd%\mingw64\bin;%PATH%
+goto rungcc
 
 :finish
 IF NOT EXIST %cd%\bin\NUL GOTO NOWINDIR
